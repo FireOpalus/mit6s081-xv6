3c5ac7e8b6cebb91be2f64f3ad150c9bcc1c3219
boiser <hy875631563@outlook.com>
2025-11-23 00:59:43 -0800

    work2

---
+++ user/pingpong.c
@@ -0,0 +1,28 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char argv[]) {
+    int pi1[2], pi2[2];
+    pipe(pi1);
+    pipe(pi2);
+    if(fork() != 0) {
+        write(pi1[1], "*", 1);
+        close(pi1[1]);
+
+        char buf;
+        read(pi2[0], &buf, 1);
+        printf("%d: received pong\n", getpid());
+    }
+    else {
+        char buf = '.';
+        read(pi1[0], &buf, 1);
+        printf("%d: received ping\n", getpid());
+        
+        write(pi2[1], "*", 1);
+        close(pi2[1]);
+    }
+    close(pi1[0]);
+    close(pi2[0]);
+    exit(0);
+}
