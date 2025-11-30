66133101bd7fad3916222a6e0c309661681bdfe7
boiser <hy875631563@outlook.com>
2025-11-29 21:48:12 -0800

    work2: using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,9 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t rw[NBUCKET];
+pthread_mutex_t m[NBUCKET];
+int read_cnt[NBUCKET];
 
 double
 now()
@@ -42,6 +45,9 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+
+  pthread_mutex_lock(&rw[i]);
+
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -53,6 +59,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&rw[i]);
 }
 
 static struct entry*
@@ -60,11 +67,21 @@ get(int key)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&m[i]);
+  if(read_cnt[i]++ == 0) {
+    pthread_mutex_lock(&rw[i]);
+  }
+  pthread_mutex_unlock(&m[i]);
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+  pthread_mutex_lock(&m[i]);
+  if(--read_cnt[i] == 0) {
+    pthread_mutex_unlock(&rw[i]);
+  }
+  pthread_mutex_unlock(&m[i]);
 
   return e;
 }
@@ -102,6 +119,11 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+  for(int i = 0; i < NBUCKET; i++) {
+    read_cnt[i] = 0;
+    pthread_mutex_init(&rw[i], NULL);
+    pthread_mutex_init(&m[i], NULL);
+  }
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
