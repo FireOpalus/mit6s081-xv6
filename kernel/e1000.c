5c399961a2531a505e85548747931aa6c5e7db6b
boiser <hy875631563@outlook.com>
2025-12-03 23:49:36 -0800

    finish

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,30 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+  acquire(&e1000_lock);
+  uint32 tdt = regs[E1000_TDT];
+  if(!(tx_ring[tdt].status & E1000_TXD_STAT_DD)) {
+    // TX ring full
+    release(&e1000_lock);
+    return -1;
+  }
+
+  // free the mbuf for the previous packet (if any)
+  if(tx_mbufs[tdt]) {
+    mbuffree(tx_mbufs[tdt]);
+    tx_mbufs[tdt] = 0;
+  }
+
+  // set up the tx descriptor
+  tx_ring[tdt].addr = (uint64) m->head;
+  tx_ring[tdt].length = m->len;
+  tx_ring[tdt].cmd = E1000_TXD_CMD_EOP | E1000_TXD_CMD_RS;
+  tx_mbufs[tdt] = m;
+
+  // advance the tail pointer
+  regs[E1000_TDT] = (tdt + 1) % TX_RING_SIZE;
+
+  release(&e1000_lock);
   return 0;
 }
 
@@ -115,6 +138,24 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  while(1) {
+    uint32 rdt = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+    if(!(rx_ring[rdt].status & E1000_RXD_STAT_DD)) {
+      // no more received packets
+      return;
+    }
+    struct mbuf *m = rx_mbufs[rdt];
+    m->len = rx_ring[rdt].length;
+    net_rx(m);
+    // allocate a new mbuf to replace the one we just gave to net_rx()
+    rx_mbufs[rdt] = mbufalloc(0);
+    if (!rx_mbufs[rdt])
+      panic("e1000");
+    rx_ring[rdt].addr = (uint64) rx_mbufs[rdt]->head;
+    rx_ring[rdt].status = 0;
+    // advance the tail pointer
+    regs[E1000_RDT] = rdt;
+  }
 }
 
 void
